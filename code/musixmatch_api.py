6c4a644c050073d0cddcf8ac03562a3787235aeb
Jeffrey Box <jeffreylbox@gmail.com>
2018-07-24 22:24:44 -0400

    musixmatch.py code linking to billboard.py

--- code/musixmatch_api.py
+++ code/musixmatch_api.py
@@ -1,4 +1,4 @@
 # musixmatch.com API
 # documentation https://developer.musixmatch.com
 
-api_key = ""
+api_key = "bdad82d08497ab6d74814e5bab5fe97c"
--- code/spotify_api.py
+++ code/spotify_api.py
@@ -1,5 +1,7 @@
 # Spotify API
 # Documentation: https://developer.spotify.com/
 
-client_id = ""
-client_secret = ""
+SPOTIPY_CLIENT_ID = "17d302e0fc9546329b3445fa434e76d6"
+SPOTIPY_CLIENT_SECRET = "8b24869e0b374115b857d820ecb9c037"
+SPOTIPY_REDIRECT_URI = "https://www.google.com/"
+username = "jeffreybox"
