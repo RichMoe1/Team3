acfbc9685787f4576691f62a427c923f45d6efc3
Haroon Ahmad <haroonahmad06@gmail.com>
2018-07-29 17:37:17 -0400

    added Spotify starter code

--- code/spotify_api.py
+++ code/spotify_api.py
@@ -1,7 +1,7 @@
 # Spotify API
 # Documentation: https://developer.spotify.com/
 
-SPOTIPY_CLIENT_ID = "17d302e0fc9546329b3445fa434e76d6"
-SPOTIPY_CLIENT_SECRET = "8b24869e0b374115b857d820ecb9c037"
+SPOTIPY_CLIENT_ID = "e80eb57e980f4b089415053d81f07790"
+SPOTIPY_CLIENT_SECRET = "79f6d99887974146a38e79556c01e4a8"
 SPOTIPY_REDIRECT_URI = "https://www.google.com/"
-username = "jeffreybox"
+username = "8wcr5g91i9s6g0sxhot64a2xn"
